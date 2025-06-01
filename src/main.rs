84841726adedf2d43d1a917f482664ece0ee1792
linbianxiaocao <awesomejie@gmail.com>
2025-06-01 10:35:13 -0700

    Derive from Deserialize trait

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 
 use std::{error::Error, fs::File, io::BufReader};
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct ServiceFabricClusters {
     #[serde(rename = "ServiceFabricCluster")]
     clusters: Vec<ServiceFabricCluster>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct ServiceFabricCluster {
     #[serde(rename = "@Name")]
     name: String,
@@ -17,13 +17,13 @@ struct ServiceFabricCluster {
     global_values: GlobalValues,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct GlobalValues {
     #[serde(rename = "EnvironmentSetting")]
     env_settings: Vec<EnvironmentSetting>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct EnvironmentSetting {
     #[serde(rename = "@Name")]
     name: String,
@@ -37,9 +37,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let clusters: ServiceFabricClusters = serde_xml_rs::from_reader(reader)?;
 
-    for cluster in clusters.clusters {
-        println!("{:?}", cluster);
+    for cluster in &clusters.clusters {
+        for env_setting in &cluster.global_values.env_settings {
+            if env_setting.name == "DstsApplicationPrincipalId" {
+                println!("{}, {}: {}", cluster.name, env_setting.name, env_setting.value);
+            }
+        }
     }
+
+    // serialize the clusters back to XML
+    let serialized = serde_xml_rs::to_string(&clusters)?;
+    println!("Serialized XML:\n{}", serialized);
 
     Ok(())
 }
